5698085c59711970e4d81f70fa41ba8be282ec17
samgg <samuel.granjeaud@inserm.fr>
2019-03-19 00:38:39 +0100

    Handle SPILL, SPILLOVER, COMP Change @data that implies keyword changes

--- app.R
+++ app.R
@@ -1,10 +1,12 @@
 # process file by file lowering memory usage
-# read only 1000 events while setting new parameters
+# read only 100 events while setting new parameters
 # original file name is used
 # $FIL is not available in some mass cytometry FCS
 
 
 if (getRversion() < "3.5.0") {
+  
+  stop("R >= 3.5.0 is required!")
 
   # installation based on biocLite  
   source("https://bioconductor.org/biocLite.R")
@@ -45,8 +47,7 @@ my.read.FCS = function(filename, which.lines = NULL) {
 
 # main function
 ChangeFCSParameters <- function(ff, dt, dir) {
-  # browser()
-  
+
   # fileName or flowFrame
   if (class(ff) == "character") {
     ofile = ifile = ff
@@ -54,28 +55,41 @@ ChangeFCSParameters <- function(ff, dt, dir) {
   } else if( class(ff) == "flowFrame") {
     ofile = ff@description$`$FIL`  # guess output file
   }
+  
+  # check the parameter translation table match the parameters
+  stopifnot(nrow(dt) == nrow(ff@parameters@data))
+  stopifnot(dt[,1:2] == ff@parameters@data[,1:2] | is.na(dt[,1:2]) | is.na(ff@parameters@data[,1:2]))
 
-  # replace parameters  
-  parindex <- grep("[0-9]S$",names(ff@description))
-  par <- gsub("S","",names(ff@description)[grep("[0-9]S$",names(ff@description))])
-  chnindex <- grep("[0-9]N$",names(ff@description))
-  # TODO: chnnew and parnam might have different length???
-  chnnew <- as.character(dt[,3])
-  parnam <- as.character(dt[par,4])
-
-  # replace parameters
-  ff@description[chnindex] <- chnnew
-  ff@description[parindex] <- parnam
-  ff@parameters@data$name  <- chnnew
-  ff@parameters@data$desc  <- parnam
+  # Following code is not used anymore with flowCore >= 1.48
+  # # replace parameters in the description
+  # chnindex <- grep("[0-9]N$",names(ff@description))
+  # parindex <- grep("[0-9]S$",names(ff@description))
+  # par <- gsub("S","",names(ff@description)[parindex])
+  # # N and S are considered in the same order
+  # # the order is given by the order of $PxS in @description
+  # ff@description[chnindex] <- as.character(dt[par,3])
+  # ff@description[parindex] <- as.character(dt[par,4])
+  
+  # replace parameters in the meta data
+  # dt is in the same order as @data by construction
+  ff@parameters@data$name  <- as.character(dt[,3])  # $PxN
+  ff@parameters@data$desc  <- as.character(dt[,4])  # $PxS
   
   # change in SPILL matrix
-  # TODO: double check the order of parameters in the matrix
   if(!is.null(colnames(ff@description$SPILL))) {
-    colnames(ff@description$SPILL) <- chnnew[row.names(dt) %in% par]
+    idx = match(colnames(ff@description$SPILL), dt[,1])
+    colnames(ff@description$SPILL) <- dt[idx,3]
+    message("SPILL changed")
+  } else if(!is.null(ff@description$SPILLOVER)) {
+    idx = match(colnames(ff@description$SPILLOVER), dt[,1])
+    colnames(ff@description$SPILLOVER) <- dt[idx,3]
+    message("SPILLOVER changed")
+  } else if(!is.null(fcs@description$COMP)) {
+    idx = match(colnames(ff@description$COMP), dt[,1])
+    colnames(ff@description$COM) <- dt[idx,3]
+    message("COMP changed")
   }
-  # TODO: SPILLOVER is ignored
-  
+
   # return a flowFrame
   if (is.null(dir)) return(ff)
   # or write a FCS file
@@ -83,9 +97,11 @@ ChangeFCSParameters <- function(ff, dt, dir) {
   if (!dir.exists(odir)) dir.create(odir)
   ofile = file.path(odir, paste0("NEW_", ofile))
   write.FCS(ff, filename = ofile)
+  message(ifile, " changed")
 }
 
-# shiny application
+# User Interface
+
 ui = fluidPage(
   titlePanel("Change the Names of Channels and Parameters in .fcs Files"),
   helpText(
@@ -105,25 +121,26 @@ ui = fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      width = 3,
+      width = 4,
       shinyDirButton("dir2","Choose input directory","Choose Input Directory"),
+      helpText("Select a directory with FCS files to process."),
       br(),br(),br(),
-      actionButton("save", "Apply NEW.name & NEW.desc to current file only"),
-      helpText("It applies names to current file and go to the next file."),
+      actionButton("save", "Apply to CURRENT file"),
+      helpText("Apply NEW.name & NEW.desc to current file and go to the next file."),
       br(),br(),
-      actionButton("all", "Apply NEW.name & NEW.desc to all files"),
+      actionButton("all", "Apply to ALL files"),
       helpText(
-        strong("Note:"), "Apply Changes to All Files: it will only execute if files ", 
-        "in the folder have the same number of parameters, i.e. same experimental panel."),
+        "Apply Changes to All Files. It will break if a file in the directory",
+        "does not have the same parameters as in the table."),
       br(),br(),
       actionButton("stop", "Stop application")
     ),
     
     mainPanel(
-      width = 9,
+      width = 8,
       fluidRow(
-        column(width = 4, actionButton("back", "Previous File")),
-        column(width = 4, actionButton("forward", "Next File"))
+        column(width = 5, actionButton("back", "Previous File")),
+        column(width = 5, actionButton("forward", "Next File"))
       ),
       fluidRow(
         column(width = 10, verbatimTextOutput("counts"))
@@ -138,16 +155,20 @@ ui = fluidPage(
   )
 )
 
+# Server side
+
 server = function(input,output){
   
   v<-reactiveValues(counts=0)
   
   setwd("~")
-  volumes=c(home=getwd(),C="C:",D="D:",E="E:",F="F:")
-  shinyDirChoose(input,id = "dir2",roots=volumes)
+  volumes = c(C="C:",D="D:",E="E:",F="F:",home=getwd())
+  shinyDirChoose(input, id = "dir2", roots = volumes)
 
+  # store path of FCS files
   dirFCS<-reactiveVal(value = NULL)
-  
+
+  # handle chooseDir  
   observeEvent(
     ignoreNULL = TRUE,
     eventExpr = {
@@ -162,6 +183,8 @@ server = function(input,output){
     }
   )
   
+  # update flowFrame from FCS
+  # store file name and flowframe of 100 events
   files<-reactive({
     if (is.null(dirFCS())) return(NULL)
     setwd(dirFCS())
@@ -170,28 +193,33 @@ server = function(input,output){
       fls<-lapply(flist, function(fileName) 
         list(
           fileName = fileName,
-          ffHeader = my.read.FCS(filename = fileName, which.lines = 1:1000)
+          ffHeader = my.read.FCS(filename = fileName, which.lines = 1:100)
         )
       )
       fls
     })
   })
   
+  # next button
   observeEvent(input$forward,{
     if (is.null(files())) return(NULL)
     v$counts<-v$counts+1
   })
   
+  # previous button
   observeEvent(input$back,{
     if (is.null(files())) return(NULL)
     if (v$counts>0) v$counts<-v$counts-1 else v$counts<-0
   })
   
+  # interactive table
   output$flow<-renderRHandsontable({
     if (is.null(files())) return(NULL)
     if (v$counts<length(files())){
+      # get parameters from flowFrame
       fs<-files()[[v$counts+1]][["ffHeader"]]
-      df<-as.data.frame.matrix(fs@parameters@data[1:2])
+      df<-as.data.frame.matrix(fs@parameters@data[,1:2])
+      # append prefilled columns
       df<-cbind(Old=df,NEW=df)
       rhandsontable(df,readOnly = T) %>% 
         hot_col("NEW.name",readOnly=F) %>% 
@@ -209,8 +237,8 @@ server = function(input,output){
       cat("Please choose a directory to start.")
     } else if ((v$counts<length(files()))) {
       cat("You are changing file: ", files()[[v$counts+1]][["fileName"]], "\n",
-          "this file is: " ,v$counts+1, " out of", length(files()),
-          " in the folder:", dirFCS())
+          "this file is: " ,v$counts+1, " out of ", length(files()),
+          " in the folder: ", dirFCS(), sep = '')
     } else {
       cat("No more files to read.\n",
           "Click previous to continue navigating, stop application or ", 
@@ -222,7 +250,7 @@ server = function(input,output){
   observeEvent(input$save,{
     if (is.null(files())) return(NULL)
     dt <- hot_to_r(input$flow)
-    if (is.na(dt)) return(NULL)
+    if (is.null(dt)) return(NULL)
     fs <- files()[[v$counts+1]][["fileName"]]
     ChangeFCSParameters(fs, dt, dirFCS())
     v$counts<-v$counts+1
@@ -232,7 +260,7 @@ server = function(input,output){
   observeEvent(input$all,{
     if (is.null(files())) return(NULL)
     dt <- hot_to_r(input$flow)
-    if (is.na(dt)) return(NULL)
+    if (is.null(dt)) return(NULL)
     withProgress(message = "Wait...Files being processed", value = 0.75, {
       lapply(files(), function(fs) ChangeFCSParameters(fs[["fileName"]], dt, dirFCS()))
       v$counts=length(files())
@@ -245,4 +273,5 @@ server = function(input,output){
   })
 }
 
+# shiny application
 shinyApp(ui=ui,server=server)
